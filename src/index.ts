0f7b42e602a5ccf9b8ba2b05973423b54b7c45f6
Bobo <bobo.kovacevic@gmail.com>
2022-03-16 11:14:56 +0100

    API documentation and versioning (#6)
    * Swagger setup
    
    * Swagger documentation update
    
    * Changed swagger output location
    
    * swagger done
    
    * host parameter
    
    * Added api routes version
    
    * added version to routes
    
    * Linter fixes

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,19 @@
 import express from 'express';
 import cors from 'cors';
+import swagger from 'swagger-ui-express';
 import container, { ContainerTypes } from './container';
 import { IControllerBase } from './controllers/IControllerBase';
+import swaggerFile from './swagger_output.json';
 
-const listenPort = process.env.PORT || 30000;
+const listenPort = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
-
-// app.use(express.json({ limit: '2mb' }));
-// app.use(
-//   express.urlencoded({
-//     extended: true,
-//   }),
-// );
-
-// app.use('/api/store', storeRouter);
-// app.use('/api/validate', validationRouter);
-// app.use('/', uiRouter);
-
-// connectApi();
 
 // Get all controllers and register all endpoints.
 const controllers: IControllerBase[] = container.getAll<IControllerBase>(ContainerTypes.Controller);
 controllers.forEach((controller) => controller.register(app));
 
+app.use('/', swagger.serve, swagger.setup(swaggerFile));
 app.listen(listenPort, () => {
     console.log('Server is listening on port ', listenPort);
 });
