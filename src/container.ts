71cef1243e9d2575a9ea10515ae5d64e25a952ca
Nikhil Ranjan <niklabh811@gmail.com>
2022-06-13 19:59:56 +0200

    Firebase serverless

--- src/container.ts
+++ src/container.ts
@@ -6,14 +6,11 @@ import { Container } from 'inversify';
 import { IControllerBase } from './controllers/IControllerBase';
 import { TokenStatsController } from './controllers/TokenStatsController';
 import { IStatsService, StatsService } from './services/StatsService';
-import { AstarApi } from './client/AstarApi';
 import { IAstarApi } from './client/BaseApi';
 import { networks } from './networks';
 import { ApiFactory, IApiFactory } from './client/ApiFactory';
 import { DappsStakingController } from './controllers/DappsStakingController';
 import { IDappsStakingService, DappsStakingService } from './services/DappsStakingService';
-import { IGasService, GasService } from './services/GasService';
-import { GasController } from './controllers/GasController';
 import { IStatsIndexerService, StatsIndexerService } from './services/StatsIndexerService';
 import { NodeController } from './controllers/NodeController';
 import { AstarApi2 } from './client/AstarApi2';
@@ -45,13 +42,11 @@ container.bind<IApiFactory>('factory').to(ApiFactory).inSingletonScope();
 container.bind<IStatsService>('StatsService').to(StatsService).inSingletonScope();
 container.bind<IDappsStakingService>('DappsStakingService').to(DappsStakingService).inSingletonScope();
 container.bind<IStatsIndexerService>('StatsIndexerService').to(StatsIndexerService).inSingletonScope();
-container.bind<IGasService>('GasService').to(GasService).inSingletonScope();
 container.bind<IFirebaseService>('FirebaseService').to(FirebaseService).inSingletonScope();
 
 // controllers registration
 container.bind<IControllerBase>(ContainerTypes.Controller).to(TokenStatsController);
 container.bind<IControllerBase>(ContainerTypes.Controller).to(DappsStakingController);
 container.bind<IControllerBase>(ContainerTypes.Controller).to(NodeController);
-container.bind<IControllerBase>(ContainerTypes.Controller).to(GasController);
 
 export default container;
