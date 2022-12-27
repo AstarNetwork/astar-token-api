a4dffdd6583fe4af9329df5faf5e1afd70c9a317
impelcrypto <impelcrypto@gmail.com>
2022-12-27 15:58:13 +0800

    feat: added xvm-transfer history questy

--- src/services/SubscanService.ts
+++ src/services/SubscanService.ts
@@ -1,28 +1,25 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from 'axios';
 import { injectable } from 'inversify';
 import { NetworkType } from '../networks';
 import { getSubscanOption, getSubscanUrl } from '../utils';
 
+export type QueryPath = 'extrinsic' | 'extrinsics';
+
 /**
  * Provides data by Subscan API
  */
 export interface ISubscanService {
-    fetchSubscan({ network, hash, type }: { network: NetworkType; hash: string; type: string }): Promise<any>;
+    fetchSubscan({ network, body, queryPath }: { network: NetworkType; body: any; queryPath: QueryPath }): Promise<any>;
 }
 
 @injectable()
 export class SubscanService implements ISubscanService {
-    public async fetchSubscan({ network, hash, type }: { network: NetworkType; hash: string; type: string }) {
+    public async fetchSubscan({ network, body, queryPath }: { network: NetworkType; body: any; queryPath: QueryPath }) {
         const base = getSubscanUrl(network);
         const option = getSubscanOption();
-        const url = base + '/api/scan/extrinsic';
-        const { data } = await axios.post(
-            url,
-            {
-                hash,
-            },
-            option,
-        );
+        const url = base + `/api/scan/${queryPath}`;
+        const { data } = await axios.post(url, body, option);
         return data.data;
     }
 }
--- src/services/TxQueryService.ts
+++ src/services/TxQueryService.ts
@@ -1,13 +1,26 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { compactAddLength, hexToU8a, u8aToHex } from '@polkadot/util';
+import { decodeAddress } from '@polkadot/util-crypto';
 import { inject, injectable } from 'inversify';
+import { IApiFactory } from '../client/ApiFactory';
 import { ContainerTypes } from '../containertypes';
-import { TransferDetails } from '../models/TxQuery';
+import { TransferDetails, XvmTxHistory } from '../models/TxQuery';
 import { NetworkType } from '../networks';
+import { SubscanExtrinsics } from './../models/TxQuery';
 import { createWeb3Instance, fetchEvmTransferDetails } from './../modules/tx-query/utils/index';
 import { networks } from './../networks';
 import { ISubscanService } from './SubscanService';
+import { ContractPromise } from '@polkadot/api-contract';
+import ABI_XVM_ERC20 from '../modules/tx-query/abi/XVM_ERC20_TRANSFER.json';
 
 export interface ITxQueryService {
     fetchTransferDetails(network: NetworkType, hash: string): Promise<TransferDetails>;
+    fetchXvmTransferHistory(
+        network: NetworkType,
+        senderAddress: string,
+        contractAddress: string,
+    ): Promise<XvmTxHistory[]>;
 }
 
 @injectable()
@@ -15,7 +28,10 @@ export interface ITxQueryService {
  * Transaction query service
  */
 export class TxQueryService implements ITxQueryService {
-    constructor(@inject(ContainerTypes.SubscanService) private _subscan: ISubscanService) {}
+    constructor(
+        @inject(ContainerTypes.SubscanService) private _subscan: ISubscanService,
+        @inject(ContainerTypes.ApiFactory) private _apiFactory: IApiFactory,
+    ) {}
 
     /**
      * Fetch the transfer transaction detail of a given hash
@@ -25,8 +41,8 @@ export class TxQueryService implements ITxQueryService {
      */
     public async fetchTransferDetails(network: NetworkType, hash: string): Promise<TransferDetails> {
         try {
-            const type = 'transfer';
-            const data = await this._subscan.fetchSubscan({ network, hash, type });
+            const body = { hash: hash };
+            const data = await this._subscan.fetchSubscan({ network, body, queryPath: 'extrinsic' });
             const isEvmTransfer = !data;
             if (isEvmTransfer) {
                 const rpc = networks[network].evmRpc;
@@ -43,6 +59,68 @@ export class TxQueryService implements ITxQueryService {
                 const symbol = asset_symbol || nativeToken;
                 return { from, to, symbol, amount, isSuccess: success, timestamp };
             }
+        } catch (e) {
+            console.error(e);
+            throw new Error('Something went wrong');
+        }
+    }
+
+    /**
+     * Fetch the xvm-transfer transaction history of a given sender and contract address
+     * @param network NetworkType (astar, shiden, rocstar or shibuya)
+     * @param senderAddress SS58 wallet address
+     * @param contractAddress SS58 XVM Transfer contract address
+     * @returns array data of the transaction history
+     */
+    public async fetchXvmTransferHistory(
+        network: NetworkType,
+        senderAddress: string,
+        contractAddress: string,
+    ): Promise<XvmTxHistory[]> {
+        try {
+            const contractPublicId = u8aToHex(decodeAddress(contractAddress));
+            const body = {
+                row: 100,
+                page: 0,
+                address: senderAddress,
+                method: 'contracts',
+                call: 'call',
+            };
+
+            const apiInstance = this._apiFactory.getApiInstance(network);
+            const [{ extrinsics }, api] = await Promise.all([
+                this._subscan.fetchSubscan({ network, body, queryPath: 'extrinsics' }),
+                apiInstance.getApiPromise(),
+            ]);
+            if (!extrinsics) return [];
+
+            const contract = new ContractPromise(api, ABI_XVM_ERC20, String(contractAddress));
+            const xvmTransfers: XvmTxHistory[] = extrinsics
+                .filter((it: SubscanExtrinsics) => {
+                    const params = JSON.parse(it.params);
+                    const contractId = params.find((it: any) => it.name === 'dest').value.Id;
+                    return contractId === contractPublicId;
+                })
+                .map((it: SubscanExtrinsics) => {
+                    const params = JSON.parse(it.params);
+                    const inputsData = params.find((it: any) => it.name === 'data').value;
+                    const decodedMessage = contract.abi.decodeMessage(compactAddLength(hexToU8a(inputsData)));
+                    const inputs = decodedMessage.args.map((arg) => arg.toString());
+
+                    const to = JSON.parse(inputs[0]);
+                    const destination = Object.values(to)[0] as string;
+                    const amount = inputs[1] as string;
+                    const erc20Address = inputs[2] as string;
+                    return {
+                        timestamp: it.block_timestamp as number,
+                        extrinsicHash: it.extrinsic_hash as string,
+                        destination,
+                        amount,
+                        erc20Address,
+                    };
+                });
+
+            return xvmTransfers;
         } catch (e) {
             console.error(e);
             throw new Error('Something went wrong');
