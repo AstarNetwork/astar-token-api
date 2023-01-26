f8ab595a5ec76f3764a4694a409a4e72f3be3bf3
Bobo <bobo.kovacevic@gmail.com>
2023-01-26 13:51:36 +0100

    Temporary disable developer social account link validation.

--- src/controllers/DappsStakingController.ts
+++ src/controllers/DappsStakingController.ts
@@ -318,6 +318,8 @@ export class DappsStakingController extends ControllerBase implements IControlle
 }
 
 export function validateDeveloperLinks(developer: Developer): boolean {
+    return true;
+
     const httpRegex =
         /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/;
 
