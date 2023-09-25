212ce52ed5053b0dd62ec29219aa0b7e0ad2c098
Bobo <bobo.kovacevic@gmail.com>
2023-09-25 13:15:45 +0200

    Lint fix

--- src/models/Nft.ts
+++ src/models/Nft.ts
@@ -1,17 +1,17 @@
 export interface NftMetadata {
-  name: string;
-  description: string;
-  image: string;
-  contractAddress: string;
-  tokenId: string;
-  tokenUri: string;
-  ownerAddress: string;
+    name: string;
+    description: string;
+    image: string;
+    contractAddress: string;
+    tokenId: string;
+    tokenUri: string;
+    ownerAddress: string;
 }
 
 export interface NftResponse {
-  items: NftMetadata[];
-  total: number;
-  page: number;
-  size: number;
-  pages: number;
+    items: NftMetadata[];
+    total: number;
+    page: number;
+    size: number;
+    pages: number;
 }
